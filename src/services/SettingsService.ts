413da24b047f71a7a222ccc22313f9664c64b15b
Joao Pedro <janesgbs@gmail.com>
2021-04-24 12:45:09 -0400

    Adicionadas modificações da aula 4

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -13,21 +13,38 @@ class SettingsService {
         this.settingsRepository = getCustomRepository(SettingsRepository);
     }
     async create({ chat, username }: ISettingsCreate){
-        const settingsRepository = getCustomRepository(SettingsRepository);
 
-        const userAlreadyExists = await settingsRepository.findOne({
+        const userAlreadyExists = await this.settingsRepository.findOne({
             username
         });
         if(userAlreadyExists){
             throw new Error("User already exists!");
         }
     
-        const settings = settingsRepository.create({
+        const settings = this.settingsRepository.create({
             chat,
             username
         });
     
-        await settingsRepository.save(settings);
+        await this.settingsRepository.save(settings);
+        return settings;
+    }
+
+    async findByUsername(username: string){
+        const settings = await this.settingsRepository.findOne({
+            username
+        });
+
+        return settings;
+    }
+
+    async update(username: string, chat: boolean){
+        await this.settingsRepository
+            .createQueryBuilder()
+            .update(Settings)
+            .set({ chat })
+            .where("username = :username", { username })
+            .execute();
     }
 }
 
